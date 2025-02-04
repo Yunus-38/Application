58a63cab09a9851eed1f6b11b5576cb3ba478101
Yunus-38 <107210213+Yunus-38@users.noreply.github.com>
2025-02-04 14:59:49 +0300

    Add project files.

---
+++ Application.Tests/CalculatorTests.cs
@@ -0,0 +1,53 @@
+﻿using Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Tests
+{
+    public class CalculatorTests
+    {
+        private readonly Calculator _calculator;
+
+        public CalculatorTests()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Fact]
+        public void Add_TwoNumbers_ReturnsCorrectSum()
+        {
+            int result = _calculator.Add(2, 3);
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void Subtract_TwoNumbers_ReturnsCorrectDifference()
+        {
+            int result = _calculator.Subtract(10, 5);
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void Multiply_TwoNumbers_ReturnsCorrectProduct()
+        {
+            int result = _calculator.Multiply(4, 3);
+            Assert.Equal(12, result);
+        }
+
+        [Fact]
+        public void Divide_ByNonZeroNumber_ReturnsCorrectQuotient()
+        {
+            int result = _calculator.Divide(10, 2);
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void Divide_ByZero_ThrowsException()
+        {
+            Assert.Throws<DivideByZeroException>(() => _calculator.Divide(10, 0));
+        }
+    }
+}
---
+++ Application.Tests/SampleTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Application.Tests
+{
+    public class SampleTests
+    {
+        [Fact]
+        public void Test_Passing()
+        {
+            Assert.True(1 + 1 == 2);
+        }
+
+        [Fact]
+        public void Test_Failing()
+        {
+            Assert.False(2 + 2 == 5);
+        }
+    }
+}
